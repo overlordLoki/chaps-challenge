a0fec3508595848f145d3d5019287ca467250e27
Jeff Lin <linyuj@ecs.vuw.ac.nz>
2022-08-27 15:48:59 +1200

    Setup stage toggle + refactor

--- src/App/App.java
+++ src/App/App.java
@@ -1,9 +1,5 @@
 package App;
 
-import Domain.*;
-import Persistency.*;
-import Recorder.*;
-import Renderer.*;
 import Renderer.Renderer;
 
 import javax.swing.*;
@@ -13,23 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static javax.swing.SwingConstants.CENTER;
-
 /**
  * Main class of the application. Includes the main method, GUI, and the main loop.
  *
  * @author Jeff Lin
  */
 public class App extends JFrame {
-    private JPanel panels;
-    private JButton buttons;
-    private JLabel labels;
-    private JPanel mazeRender;
+    private static final String FONT = "Agency FB";
+    private static final int STYLE = Font.BOLD;
     private Controller controller;
     private List<String> keyBindings = new ArrayList<>(List.of("Up","Down","Left","Right","Space","Esc","1","2","X","S","R","Ctrl"));
     private List<String> keyNames = List.of("Up","Down","Left","Right","Space","Esc","1","2","X","S","R","Ctrl");
     private int settingKey = -1;
     private Runnable closePhase = ()->{};
+    private List<Runnable> stages = new ArrayList<>();
 
     /**
      * Constructor for the App class. Initializes the GUI and the main loop.
@@ -48,51 +41,53 @@ public class App extends JFrame {
 
     private void menuScreen(){
         var pnOuterMost = new JPanel();
-
         var jlWelcome = new JLabel("Chaps Challenge!");
         List<JLabel> labels = new ArrayList<>(List.of(
-                new JLabel("Start!"),
-                new JLabel("Load"),
+                new JLabel("Start New Game!"),
+                new JLabel("Load Game"),
                 new JLabel("Settings"),
                 new JLabel("How to play"),
                 new JLabel("Credits"),
                 new JLabel("Exit"))
         );
-
+        activateStages();
         // setting layout
         pnOuterMost.setLayout(new BoxLayout(pnOuterMost, BoxLayout.Y_AXIS));
+        pnOuterMost.setBackground(Color.PINK);
         jlWelcome.setAlignmentX(CENTER_ALIGNMENT);
-        jlWelcome.setFont(new Font("Agency FB", Font.BOLD, 80));
-
+        jlWelcome.setFont(new Font(FONT, STYLE, 80));
 
         // assemble this frame
         pnOuterMost.add(jlWelcome);
         pnOuterMost.add(Box.createVerticalGlue());
-        labels.forEach(l->{
-            l.addMouseListener(new MouseAdapter() {
-                public void mouseEntered(MouseEvent e){l.setForeground(Color.RED);}
-                public void mouseExited(MouseEvent e) {l.setForeground(Color.BLACK);}
-                public void mouseClicked(MouseEvent e) {
-                    // trigger panel switching
-                }
-            });
-            l.addKeyListener(new KeyAdapter() {
-                public void keyReleased(KeyEvent e) {
-                    // trigger key binding
-                }
-            });
-            l.setAlignmentX(CENTER_ALIGNMENT);
-            l.setFont(new Font("Agency FB", Font.BOLD, 40));
-            pnOuterMost.add(l);
-        });
+        setLabelsAndAttachToPanel(pnOuterMost, labels);
         add(pnOuterMost);
-
 
         closePhase.run();
         closePhase = () -> remove(pnOuterMost);
         setPreferredSize(new Dimension(1200, 600));
         pack();
     }
+
+
+
+    private void setLabelsAndAttachToPanel(JPanel pnOuterMost, List<JLabel> labels) {
+        labels.forEach(l->{
+            l.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent e){l.setForeground(Color.RED);}
+                public void mouseExited(MouseEvent e) {l.setForeground(Color.BLACK);}
+                public void mousePressed(MouseEvent e) {
+                    // trigger panel switching
+                    stages.get(labels.indexOf(l)).run();
+                }
+            });
+            l.setAlignmentX(CENTER_ALIGNMENT);
+            l.setFont(new Font(FONT, STYLE, 40));
+            pnOuterMost.add(l);
+        });
+    }
+
+
 
     private void level(){
         // need to invoke persistent package to create level here
@@ -141,8 +136,51 @@ public class App extends JFrame {
     }
 
 
+    //================================================================================================================//
+    //============================================= Menu Stages ======================================================//
+    //================================================================================================================//
+
+    private void activateStages(){
+        stages.add(this::stageNewGame);
+        stages.add(this::stageLoad);
+        stages.add(this::stageSettings);
+        stages.add(this::stageInstructions);
+        stages.add(this::stageCredits);
+        stages.add(this::stageExit);
+    }
+
+    private void stageNewGame() {
+        System.out.println("New Game");
+    }
+
+    private void stageLoad() {
+        System.out.println("Load Game");
+    }
+
+    private void stageSettings() {
+        System.out.println("Settings");
+    }
+
+    private void stageInstructions() {
+        System.out.println("How to play");
+    }
+
+    private void stageCredits() {
+        System.out.println("Credits");
+    }
+
+    private void stageExit() {
+        if (JOptionPane.showOptionDialog(this,
+                "Are you sure you want to quit the game?", "Select an Option" ,
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null) == 0){
+            System.exit(0);
+        }
+    }
 
 
+    //================================================================================================================//
+    //============================================= Main Method ======================================================//
+    //================================================================================================================//
 
     /**
      * Main method of the application.
