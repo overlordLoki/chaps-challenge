ea5af73d75d6509db6f204754453413c1112d837
Loki <anderstymo@ecs.vuw.ac.nz>
2022-08-28 15:28:23 +1200

    update to mock
    to better reflect whos job it is

--- src/Renderer/tempDomain/Maze.java
+++ src/Renderer/tempDomain/Maze.java
@@ -10,8 +10,15 @@ public class Maze {
         return gameArray;
     }
 
-    public Maze(int width, int height) {
+    public Maze() {
+        int width = 10;
+        int height = 10;
         gameArray = new Tile[width][height];
-        
+        //initialize the maze with Empty_tile. cant have null tiles
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                gameArray[i][j] = new Empty_tile();
+            }
+        }
     }
 }
