ec69a932bd71d50cc9ef632e1ef4f2c9a29a3bd7
Loki <anderstymo@ecs.vuw.ac.nz>
2022-08-28 14:46:35 +1200

    added method for changing texture packs

--- src/Renderer/Renderer.java
+++ src/Renderer/Renderer.java
@@ -2,7 +2,11 @@ package Renderer;
 import Domain.*;
 import Persistency.*;
 import Persistency.Tiles.Tile;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class Renderer extends JPanel{
     static final long serialVersionUID = 1L;
@@ -12,5 +16,20 @@ public class Renderer extends JPanel{
     private Tile[][] gameArray;
     //maze
     private Maze maze;
-    
+
+    public Renderer(Maze maze) {
+        this.maze = maze;
+    }
+
+    public BufferedImage setTexturePack(String texturePack) {
+        this.texturePack = texturePack;
+        try {
+            return ImageIO.read(getClass().getResource("/render/textures/" + texturePack + "/background.png"));
+        } catch (IOException e) {throw new RuntimeException(e);}
+    }
+
+    public Renderer getWindow(){
+        return this;
+    }
+
 }
