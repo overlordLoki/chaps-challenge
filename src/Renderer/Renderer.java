9aa1fa741776fcdaf2b8920f43de69c7df0be2db
Loki <anderstymo@ecs.vuw.ac.nz>
2022-08-28 14:39:24 +1200

    initial

--- src/Renderer/Renderer.java
+++ src/Renderer/Renderer.java
@@ -1,6 +1,16 @@
 package Renderer;
-
 import Domain.*;
+import Persistency.*;
+import Persistency.Tiles.Tile;
+import javax.swing.JPanel;
 
-public class Renderer {
+public class Renderer extends JPanel{
+    static final long serialVersionUID = 1L;
+    //default texturePack
+    private String texturePack = "Original";
+    //maze array
+    private Tile[][] gameArray;
+    //maze
+    private Maze maze;
+    
 }
