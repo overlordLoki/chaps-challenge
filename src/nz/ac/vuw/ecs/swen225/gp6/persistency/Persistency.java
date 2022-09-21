023554d6b62fd930562ba1f900b9722ed7a740ba
Benjamin Hong <hongbenj@ecs.vuw.ac.nz>
2022-09-21 12:32:23 +1200

    Temporarily comment out Persistency code using old Domain

--- src/nz/ac/vuw/ecs/swen225/gp6/persistency/Persistency.java
+++ src/nz/ac/vuw/ecs/swen225/gp6/persistency/Persistency.java
@@ -11,42 +11,42 @@ import org.dom4j.Element;
 import nz.ac.vuw.ecs.swen225.gp6.app.*;
 
 public class Persistency {
-    /*
-     * Serialise a domain to an XML document
-     * 
-     * @param domain The domain to serialise
-     * 
-     * @return The serialised domain as an XML document
-     */
-    public static Document serialize(Domain domain) {
-        Document document = DocumentHelper.createDocument();
-        Element root = document.addElement("Domain");
-        return document;
-    }
+    // /*
+    // * Serialise a domain to an XML document
+    // *
+    // * @param domain The domain to serialise
+    // *
+    // * @return The serialised domain as an XML document
+    // */
+    // public static Document serialize(Domain domain) {
+    // Document document = DocumentHelper.createDocument();
+    // Element root = document.addElement("Domain");
+    // return document;
+    // }
 
-    /*
-     * Unserialise a domain to a file
-     * 
-     * @param xml The XML document to unserialise
-     * 
-     * @param domain The domain to unserialise to
-     */
-    public static Domain unserialize(String xml) {
-        return new Domain();
-    }
+    // /*
+    // * Unserialise a domain to a file
+    // *
+    // * @param xml The XML document to unserialise
+    // *
+    // * @param domain The domain to unserialise to
+    // */
+    // public static Domain unserialize(String xml) {
+    // return new Domain();
+    // }
 
-    /*
-     * Save a domain to a file
-     * 
-     * @param domain The domain to save
-     * 
-     * @param path The file path to save to
-     */
-    public static void save(Domain domain, String path) throws IOException {
-        Document document = serialize(domain);
+    // /*
+    // * Save a domain to a file
+    // *
+    // * @param domain The domain to save
+    // *
+    // * @param path The file path to save to
+    // */
+    // public static void save(Domain domain, String path) throws IOException {
+    // Document document = serialize(domain);
 
-        FileWriter out = new FileWriter(path);
-        document.write(out);
-        out.close();
-    }
+    // FileWriter out = new FileWriter(path);
+    // document.write(out);
+    // out.close();
+    // }
 }
