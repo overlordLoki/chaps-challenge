62df43626eac1053818fc0023a78e4f63f57e3b7
hooperjayd <hooperjayd@myvuw.ac.nz>
2022-09-23 16:17:11 +1200

    Commented out ReplayPanel as it conflicts.

--- src/nz/ac/vuw/ecs/swen225/gp6/recorder/ReplayPanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp6/recorder/ReplayPanel.java
@@ -3,13 +3,12 @@ package nz.ac.vuw.ecs.swen225.gp6.recorder;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageOutputStream;
 import javax.swing.BoxLayout;
+import java.awt.*;
 import javax.swing.ImageIcon;
-
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-
-import nz.ac.vuw.ecs.swen225.gp6.app.PanelCreator;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -22,44 +21,36 @@ import java.awt.event.MouseEvent;
 
 
 public class ReplayPanel extends JPanel {
-    static final File dir = new File("src\\nz\\ac\\vuw\\ecs\\swen225\\gp6\\recorder\\innerrecorder\\images");
+    // static final File dir = new File("src\nz\ac\vuw\ecs\swen225\gp6\recorder\innerrecorder\images");
     List<BufferedImage> imgs = new ArrayList<BufferedImage>();
-    JPanel panel;
 
-    public ReplayPanel(){
-        panel = PanelCreator.createClearPanel(BoxLayout.X_AXIS);
-        if(imgs.isEmpty()) {loadImages();}
-        imgs.forEach(img -> {
-            JLabel label = new JLabel(new ImageIcon(img));
-            System.out.println("making replay labels");
-            label.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    System.out.println("clicked " + img);
-                }
-                
-            });
-            panel.add(label);
-        });
-        imgs.forEach(img -> panel.add(new JLabel(new ImageIcon(img))));
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e){System.out.println("mouse entered");}
-            @Override
-            public void mouseExited(MouseEvent e) {System.out.println("mouse exit");}
-            @Override
-            public void mousePressed(MouseEvent e) {System.out.println("mouse pressed");}
-        });
-    }
+    // public ReplayPanel(){
+    //     if(imgs.isEmpty()) {loadImages();}
+    //     this.setLayout(new GridLayout(2,2));
+    //     imgs.forEach(img -> {
+    //         JButton label = new JButton(new ImageIcon(img));
+    //         System.out.println("making " + imgs.indexOf(img) + " labels");
+    //         label.addMouseListener(new MouseAdapter() {
+    //             @Override
+    //             public void mousePressed(MouseEvent e) {
+    //                 System.out.println("clicked " + img);
+    //             }
+    //         });
+    //         this.add(label);
+    //     });
+    // }
 
-    public void loadImages(){
-        if(!dir.isDirectory()) {throw new IllegalStateException("Not a directory");}
-        Stream.of(dir.listFiles()).forEach(f -> {
-            try {
-                imgs.add(ImageIO.read(f));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-    }
+    /**
+    //  * Loads all images from the images folder into the imgs list.
+    //  */
+    // public void loadImages(){
+    //     if(!dir.isDirectory()) {throw new IllegalStateException("Not a directory");}
+    //     Stream.of(dir.listFiles()).forEach(f -> {
+    //         try {
+    //             imgs.add(ImageIO.read(f));
+    //         } catch (IOException e) {
+    //             e.printStackTrace();
+    //         }
+    //     });
+    // }
 }
