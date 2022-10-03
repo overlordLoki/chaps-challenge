922cc3ef24f209b951a65626b7565b8b3caedc7a
Mahdi Najafi <najafimahd@ecs.vuw.ac.nz>
2022-10-03 15:53:17 +1300

    hero and enemy static direction will start with Direction.Up, CheckGame for end game state added
    staticDirection should never be None or null

--- src/nz/ac/vuw/ecs/swen225/gp6/domain/Tiles/Enemy.java
+++ src/nz/ac/vuw/ecs/swen225/gp6/domain/Tiles/Enemy.java
@@ -1,13 +1,13 @@
 package nz.ac.vuw.ecs.swen225.gp6.domain.Tiles;
 
-import nz.ac.vuw.ecs.swen225.gp6.domain.Domain;
+import nz.ac.vuw.ecs.swen225.gp6.domain.*;
 import nz.ac.vuw.ecs.swen225.gp6.domain.IntegrityCheck.*;
 import nz.ac.vuw.ecs.swen225.gp6.domain.TileAnatomy.*;
 import nz.ac.vuw.ecs.swen225.gp6.domain.TileGroups.*;
 import nz.ac.vuw.ecs.swen225.gp6.domain.Utility.*;
 
 public class Enemy extends Actor{
-    Direction staticDirection; //TODO implement this
+    Direction staticDirection = Direction.Up; //should never be None
     Tile tileOn; //tile the enemy will replace when moved
     
     public Enemy(TileInfo info){super(info);}
@@ -17,6 +17,7 @@ public class Enemy extends Actor{
     public Direction dir(){return staticDirection;} //returns the direction the hero is facing
     
     @Override public boolean damagesHero(Domain d){return true;}
+    @Override public Tile replaceWith(){return this;} 
     @Override public void setOn(Tile t, Domain d){
         d.getCurrentMaze().setTileAt(info.loc(), t);
 
@@ -24,10 +25,28 @@ public class Enemy extends Actor{
         if(t.type() == TileType.Hero){
             d.getEventListener(Domain.DomainEvent.onLose).forEach(r -> r.run());
             CheckGame.gameHasEnded = true; //let the integrity checker know the game has ended
+            CheckGame.won = false;
         }
     }
     
-    @Override public void ping(Domain d){} //TODO: figure out how to make ben override/make all this
+    @Override public void ping(Domain d){
+        Maze m = d.getCurrentMaze();
+        Loc l1 = info.loc();
+        Direction dir = Direction.values()[(int)(Math.random()*4)]; //random direction
+        Loc l2 = dir.transformLoc(l1); //new loc 
+        Tile tileToOccupy = m.getTileAt(l2); //tile enemy is to move on to
+
+        //if enemy hasnt moved or tile at new location is obstruction return
+        if(dir == Direction.None || tileToOccupy.obstructsEnemy(d)) return;
+
+        m.getTileAt(l1).setOn(tileOn, d); //set previous location to tileOn
+        m.getTileAt(l2).setOn(this, d);   //set new location to enemy, NOTE: Order matters here!
+
+        this.tileOn = tileToOccupy.replaceWith(); // set tile heros to replace when moving off
+        staticDirection = m.getDirection(); //set heros direction of facing 
+    } 
+    
+    //TODO: figure out how to make ben override/make all this
 
     
 }
