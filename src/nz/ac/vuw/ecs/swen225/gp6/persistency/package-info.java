2b1d78b74c52544dc8774aa16755301f3bb39297
Benjamin Hong <hongbenj@ecs.vuw.ac.nz>
2022-10-16 15:32:00 +1300

    Update Persistency Javadocs

---
+++ src/nz/ac/vuw/ecs/swen225/gp6/persistency/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the utility classes necessary to persist game saves, configuration, and recordings.
+ *
+ * @author Benjamin Hong - 300605520
+ */
+package nz.ac.vuw.ecs.swen225.gp6.persistency;
